4ec39fb6bc52cd88a507bf078e3d548b9d858e6a
Nicholas Valbusa <squallstar@gmail.com>
2016-02-21 15:06:14 +0000

    add new block

---
+++ client/lib/constants.js
@@ -0,0 +1,6 @@
+BLOCK_TYPES = [
+  { type: 'transport', label: 'Transport' },
+  { type: 'event', label: 'Event' },
+  { type: 'todo', label: 'Todo list' },
+  { type: 'stay', label: 'Stay' }
+];
--- client/templates/trip/trip-create-block.js
+++ client/templates/trip/trip-create-block.js
@@ -1,10 +1,5 @@
 Template.tripCreateBlock.helpers({
   types : function () {
-    return [
-      { type: 'transport', label: 'Transport' },
-      { type: 'event', label: 'Event' },
-      { type: 'todo', label: 'Todo list' },
-      { type: 'stay', label: 'Stay' }
-    ];
+    return BLOCK_TYPES;
   }
 });
